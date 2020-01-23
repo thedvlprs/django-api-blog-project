40e96d3609e5c091bd34316d2624fa7e0abfe322
Fyunka <fyunkafinka@hotmail.com>
2020-01-23 17:50:21 +0500

    feat: add Project-level Permission and Custom permissions

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
