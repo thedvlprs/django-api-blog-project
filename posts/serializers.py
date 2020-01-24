94684a0584ade3f14bed18892e09cbdc9771359e
Fyunka <fyunkafinka@hotmail.com>
2020-01-24 16:14:01 +0500

    feat: add ViewSets and Routers to reduce amount of code

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
@@ -6,5 +7,14 @@ class PostSerializer(serializers.ModelSerializer):
     
     
     class Meta:
-        fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+        fields = ('id', 'author', 'title', 'body', 'created_at',)
+        
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    
+    
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
